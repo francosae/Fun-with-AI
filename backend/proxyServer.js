9e3aecc97f211054c5641a7e30083bf0b4267a80
Paul Franco <paulfranco.e@gmail.com>
2022-05-18 03:59:00 -0400

    First Commit

---
+++ backend/proxyServer.js
@@ -0,0 +1,42 @@
+var express = require('express');
+var cors = require('cors');
+const axios = require('axios');
+var app = express();
+require('dotenv').config()
+app.use(cors());
+
+
+
+const { Configuration, OpenAIApi } = require("openai");
+
+const configuration = new Configuration({
+  apiKey: process.env.OPENAI_API_KEY,
+});
+
+const openai = new OpenAIApi(configuration);
+
+
+app.get('/sendPrompt', async (req, res) => {
+  const prompt = req.query.prompt;
+  const response = await openai.createCompletion("text-curie-001", {
+    prompt: prompt,
+    temperature: 0.7,
+    max_tokens: 256,
+    top_p: 1,
+    frequency_penalty: 0,
+    presence_penalty: 0,
+  });
+  let responseObj = {
+    apiResponse: response.data.choices[0],
+    sentPrompt: prompt,
+    executed: true
+  }
+  console.log(responseObj)
+  res.send(responseObj)
+})
+
+
+app.listen(4000, function () {
+    console.log("Server started on port 4000");
+});
+
